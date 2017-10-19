13acd6f58d80b3892a4d6ce09efd5bfd00df2b03
Patrick McClernan <patmccler@gmail.com>
2017-10-19 23:54:52 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 
 def position_taken?(board, position)
 
-  if( board[position] == "" || board[position] == " " || board[position] = nil)
+  if( board[position] == "" || board[position] == " " || board[position] == nil)
     return false
   end
   return true
